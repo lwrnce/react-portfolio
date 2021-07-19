9deaceafc72f2cd6defc4338db0994622d4c589d
lwrnce <lwrnceyuen@gmail.com>
2021-07-18 19:46:17 -0700

    create useState for selecting pages app.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ import Project from "./components/Project";
 import ContactForm from "./components/Contact";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("about");
+
+  const selectPage = () => {
+    switch (currentPage) {
+      case "projects":
+        return <Projects />;
+      case "contact":
+        return <ContactForm />;
+      default:
+        return <About />;
+    }
+  };
+
   return (
     <div>
-      <Header></Header>
+      <Header
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        />
       <main>
-        {/* <div>{currentPage(page)}</div> */}
+        {selectPage(currentPage)}
       </main>
       <Footer></Footer>
     </div>
