6567dd3fafbcaff249d211a206e1f4f13ded67df
lwrnce <lwrnceyuen@gmail.com>
2021-07-18 18:43:10 -0700

    create about component and unfilled  placeholder components

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import profileImage from "../../assets/profile/profile.jpg";
+
+function About() {
+
+  return (
+    <section className="my-5" >
+      <h1 id="about">About Me</h1>
+      <img src={profileImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+        Hello! My name is Lawrence Yuen and welcome to my website. I recently graduated in 2020 from UC Irvine with a Bachelor's in anthropology.
+        I enjoy learning to code during my free time and have started UC Berkeley's Coding Bootcamp.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default About
