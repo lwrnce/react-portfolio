d55f97b5fa1b844fb1aa266590ffb8860975ac57
lwrnce <lwrnceyuen@gmail.com>
2021-07-18 20:41:33 -0700

    create project tile component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
-import Project from "./components/Project";
+import Projects from "./components/Project";
 import ContactForm from "./components/Contact";
 
 function App() {
