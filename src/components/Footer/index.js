79c0d2a2461fc05683d3aad1a006065a5008fffa
lwrnce <lwrnceyuen@gmail.com>
2021-07-18 20:43:51 -0700

    creat footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div style={{ justifyContent: "space-evenly"}}>
+                    <h5 class="text-muted">Thanks for visiting!</h5>
+                    <p class="copyright text-muted">
+                        Copyright ©2020 Lawrence Yuen | All rights reserved
+                    </p>
+      </div>
+    </footer>
+  );
+
+}
+
+export default Footer;
